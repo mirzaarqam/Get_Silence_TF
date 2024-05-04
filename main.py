6f259250b40919aeee7c707b2c547d5aef22517e
Arqam.Mirza <mirzaarqambaig@gmail.com>
2024-05-04 11:56:51 +0500

    just empty Project

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from pydub import AudioSegment
+import numpy as np
+def is_silent(audio_file_path):
 
+    my_sound = AudioSegment.from_file(audio_file_path)
+    audio_sample = np.array(my_sound.get_array_of_samples())
+    dBFS = 20 * np.log10(np.max(np.abs(audio_sample)) / (2 ** 15))
+    threshold = -40
+    return dBFS <= threshold
+
+if __name__ == "__main__":
+    audio_file_path = "silent.wav"
+    if is_silent(audio_file_path):
+        print("The audio file is silent.")
+    else:
+        print("The audio file is not silent.")
